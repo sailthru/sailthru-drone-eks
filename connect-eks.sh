2ca8fa3526fd7effb712c9811dc795cf78182383
Jean-Baptiste Farez <jb@heetch.com>
2019-03-13 15:23:52 +0100

    Update connect script with homogenous values

--- connect-eks.sh
+++ connect-eks.sh
@@ -7,8 +7,10 @@ ci_role=${PLUGIN_USE_CI_ROLE:-'ci'}
 session_id="${DRONE_COMMIT_SHA:0:10}-${DRONE_BUILD_NUMBER}"
 account_id=${PLUGIN_ACCOUNT:-'none'}
 aws_credentials_ttl=${PLUGIN_AWS_CREDENTIALS_TTL:-'3600'}
-aws_region=${PLUGIN_AWS_REGION:-'eu-west-1'}
+aws_region=${PLUGIN_AWS_REGION}
 on_error=${PLUGIN_ON_ERROR:-'cleanup'}
+role_arn="arn:aws:iam::${account_id}:role/${ci_role}"
+eks_cluster=${PLUGIN_EKS_CLUSTER:-'NOT_SET'}
 
 # Manage mult-account
 #
@@ -22,6 +24,17 @@ echo "  CI Role: ${ci_role}"
 echo "  Account ID: ${account_id}"
 echo "  IAM Role Session ID: ${session_id}"
 echo "  IAM Credentials TTL: ${aws_credentials_ttl}"
+
+# Check the aws_region is set. If not, retrieve it.
+if [ -z ${aws_region} ]; then
+  # Try to pull the region from the host that is running Drone - this assumes
+  # the Drone EC2 instance is in the same region as the EKS cluster you are
+  # deploying onto. If needed, override with PLUGIN_AWS_REGION param,
+  export aws_region_and_zone=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+  export aws_region=`echo ${aws_region_and_zone} | sed 's/[a-z]$//'`
+
+fi
+export AWS_DEFAULT_REGION=${aws_region}
 
 # Get authentified if a role is specified
 if [ "${account_id}" != "IAM Role" ]; then
@@ -39,43 +52,26 @@ fi
 
 # Establish an authenticated connection to an EKS cluster.
 #
-if [ -z ${PLUGIN_EKS_CLUSTER} ]; then
-  echo "EKS_CLUSTER (Name of EKS cluster) must be defined."
+if [ -z ${eks_cluster} ]; then
+  echo "PLUGIN_EKS_CLUSTER (Name of EKS cluster) must be defined."
   exit 1
 fi
-
-if [ -z ${PLUGIN_IAM_ROLE_ARN} ]; then
-  echo "IAM_ROLE_ARN (ARN of the IAM role with cluster deploy/management perms) must be defined."
-  exit 1
-fi
-
-if [ -z ${PLUGIN_AWS_REGION} ]; then
-  # Try to pull the region from the host that is running Drone - this assumes
-  # the Drone EC2 instance is in the same region as the EKS cluster you are
-  # deploying onto. If needed, override with PLUGIN_AWS_REGION param,
-  export AWS_REGION_AND_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
-  export PLUGIN_AWS_REGION=`echo ${AWS_REGION_AND_ZONE} | sed 's/[a-z]$//'`
-
-fi
-export AWS_DEFAULT_REGION=${PLUGIN_AWS_REGION}
-
-
 
 # Fetch the token from the AWS account.
-KUBERNETES_TOKEN=$(aws-iam-authenticator token -i $PLUGIN_EKS_CLUSTER -r $PLUGIN_IAM_ROLE_ARN | jq -r .status.token)
-
-if [ -z $KUBERNETES_TOKEN ]; then
-  echo "Unable to obtain Kubernetes token - check Drone's IAM permissions"
-  echo "Maybe it cannot assume the ${PLUGIN_IAM_ROLE_ARN} role?"
-  exit 1
-fi
+#KUBERNETES_TOKEN=$(aws-iam-authenticator token -i $eks_cluster -r $role_arn | jq -r .status.token)
+#
+#if [ -z $KUBERNETES_TOKEN ]; then
+#  echo "Unable to obtain Kubernetes token - check Drone's IAM permissions"
+#  echo "Maybe it cannot assume the ${role_arn} role?"
+#  exit 1
+#fi
 
 
 # Fetch the EKS cluster information.
-EKS_URL=$(aws eks describe-cluster --name ${PLUGIN_EKS_CLUSTER} | jq -r .cluster.endpoint)
-EKS_CA=$(aws eks describe-cluster --name ${PLUGIN_EKS_CLUSTER} | jq -r .cluster.certificateAuthority.data)
+eks_url=$(aws eks describe-cluster --name ${eks_cluster} | jq -r .cluster.endpoint)
+eks_ca=$(aws eks describe-cluster --name ${eks_cluster} | jq -r .cluster.certificateAuthority.data)
 
-if [ -z $EKS_URL ] || [ -z $EKS_CA ]; then
+if [ -z $eks_url ] || [ -z $eks_ca ]; then
   echo "Unable to obtain EKS cluster information - check Drone's EKS API permissions"
   exit 1
 fi
@@ -90,20 +86,20 @@ kind: Config
 
 clusters:
 - cluster:
-    server: ${EKS_URL}
-    certificate-authority-data: ${EKS_CA}
-  name: eks_${PLUGIN_EKS_CLUSTER}
+    server: ${eks_url}
+    certificate-authority-data: ${eks_ca}
+  name: eks_${eks_cluster}
 
 contexts:
 - context:
-    cluster: eks_${PLUGIN_EKS_CLUSTER}
-    user: eks_${PLUGIN_EKS_CLUSTER}
-  name: eks_${PLUGIN_EKS_CLUSTER}
+    cluster: eks_${eks_cluster}
+    user: eks_${eks_cluster}
+  name: eks_${eks_cluster}
 
-current-context: eks_${PLUGIN_EKS_CLUSTER}
+current-context: eks_${eks_cluster}
 
 users:
-- name: eks_${PLUGIN_EKS_CLUSTER}
+- name: eks_${eks_cluster}
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1alpha1
@@ -111,7 +107,7 @@ users:
       args:
         - "token"
         - "-i"
-        - ${PLUGIN_EKS_CLUSTER}
+        - ${eks_cluster}
         - -r
-        - ${PLUGIN_IAM_ROLE_ARN}
+        - ${role_arn}
 EOF
